1bf0a1886ad9c1fd7230a371ed67888604aba19d
james cotter <capricorngod98@smail.astate.edu>
2019-10-01 15:45:48 -0500

    added a search bar

--- tableview/ViewController.swift
+++ tableview/ViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource,UISearchBarDelegate {
     
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var myTableView: UITableView!
-    var data = ["tools",]
+    let data = ["tools",]
+    
+    var filteredData: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         myTableView.dataSource = self
+        searchBar.delegate = self
+        filteredData = data
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
@@ -28,6 +33,12 @@ class ViewController: UIViewController, UITableViewDataSource {
         myCell?.textLabel?.text = holderText
         return myCell!
     }
-
+    func searchBar(_searchBar: UISearchBar, textDidChange searchText: String)
+    {
+        filteredData = searchText.isEmpty ? data : data.filter{(item: String) -> Bool in
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        myTableView.reloadData()
+    }
 }
 
